ca34af85508a4b2901fd1948f461b5aed88ebab6
Jonathan Sanz Carcelen <jonathan.sanz14@outlook.com>
2025-07-17 13:35:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 # Stripe configuration
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
-DOMAIN = "http://localhost:8501"  # Update this to your deployed domain
+DOMAIN = "https://helloappapp.streamlit.app/"  # Update this to your deployed domain
 
 # Load products
 @st.cache_data
